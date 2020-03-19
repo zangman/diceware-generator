940a08e50f80b51c960121fe0cbc58aee4b7ca74
zangman <47423037+zangman@users.noreply.github.com>
2020-03-20 03:31:27 +0800

    Missed a pragma.

--- src/main_window.h
+++ src/main_window.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 #include "diceware.h"
 #include <gtkmm/aboutdialog.h>
 #include <gtkmm/applicationwindow.h>
